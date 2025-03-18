28967676978c514259b9942437122c7f3f564567
Michael Miller <michael_miller@msn.com>
2025-03-18 13:06:24 -0400

    added license

--- write_thumbnail_xmp.lua
+++ write_thumbnail_xmp.lua
@@ -1,12 +1,29 @@
--- Darktable Lua script to write a thumbnail to the related XMP metadata files
+-- Darktable Lua script to write a thumbnail to the related XMP metadata file
 -- so digiKam can display the thumbnail in the digiKam Light Table view.
+-- 
+-- Copyright (C) 2025  Michael Miller <michael underscore miller at msn dot com>
+--
+-- MIT License
+-- 
+-- Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+-- documentation files (the “Software”), to deal in the Software without restriction, including without limitation
+-- the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and
+-- to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all copies or substantial portions
+-- of the Software.
+--   
+-- THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+-- TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+-- TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
--- Add the custom path to the Lua library search paths
-package.path = package.path .. ";/opt/hb-digikam.org.arm64/Cellar/luarocks/3.11.1/share/lua/5.4/?.lua"
-package.cpath = package.cpath .. ";/opt/hb-digikam.org.arm64/lib/lua/5.4/?.so;/opt/hb-digikam.org.arm64/lib/lua/5.4/mime/?.so"
+
+-- Uncomment the following 2 lines and replace with your values if the Lua library environment variables are not set
+-- package.path = package.path .. ";/opt/hb-digikam.org.arm64/Cellar/luarocks/3.11.1/share/lua/5.4/?.lua"
+-- package.cpath = package.cpath .. ";/opt/hb-digikam.org.arm64/lib/lua/5.4/?.so;/opt/hb-digikam.org.arm64/lib/lua/5.4/mime/?.so"
 
 local dt = require "darktable"
-local dtdbg = require "lib/dtutils.debug"
 local mime = require "mime"
 local gettext = dt.gettext.gettext
 
